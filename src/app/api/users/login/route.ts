ce6fa5be63e8a68b3c96cf3408da23cd5461df51
hrithik7-class <kesarwaniraj11@gmail.com>
2025-08-21 13:12:34 +0530

    signup route done

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -1,23 +1,70 @@
-import {Db} from "../../../../dbConfig/dbConfig";
-import {User} from "../../../../models/usermodel";
-import { NextRequest , NextResponse } from "next/server";
-import bcrypt from "bcryptjs";
-
-
+import { Db } from "../../../../dbConfig/dbConfig";
+import User from "../../../../models/usermodel";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 Db();
 
-export async function POST(resquest:NextRequest) {
+export async function POST(request: NextRequest) {
     try {
-        const reqBody = await resquest.json();
-        const {username , email , password} = reqBody
+        const reqBody = await request.json();
+        const { email, password } = reqBody;
         console.log(reqBody);
         //if user has 
+        if (!email || !password) {
+            return NextResponse.json(
+                { message: "Please fill the login field." },
+                { status: 400 }
+            );
+        }
+        const user = await User.findOne({ email });
+        if (!user) {
+            return NextResponse.json(
+                { message: "User not found" },
+                { status: 400 }
+            );
+        }
 
-        
+        const validPassword = bcryptjs.compare(password, user.password)
+
+        if (!validPassword) {
+            return NextResponse.json(
+                { message: "Password is incorrect" },
+                { status: 400 }
+            );
+        }
+        console.log("hello 1")
+        const token = jwt.sign(
+            { id: user._id, email: user.email },
+            process.env.SECRET_TOKEN!,
+            { expiresIn: "7d" }
+        );
+
+        const response = NextResponse.json({
+            message: "User login successfully",
+            success: true,
+            user: {
+                _id: user._id,
+                email: user.email
+            }
+        });
+
+        response.cookies.set("token", token, {
+            httpOnly: true,
+        });
+
+        return response;
+
     } catch (error) {
-        return NextResponse.json({error:"error is post request"},{status:500});
-        
+        return NextResponse.json({
+            error: "error is post request"
+        },
+            {
+                status: 500
+
+            }
+        );
     }
-    
+
 }
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,24 +4,50 @@ import Link from "next/link";
 import React from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
+import toast from "react-hot-toast";
 
 
-export default function LoginPage(){
-    const [user ,setUser]= React.useState({
-        email:"",
-        password:""
+export default function LoginPage() {
+    const router = useRouter();
+    const [buttonDisable, setButtonDisable] = React.useState(false);
+    const [loading, setLoading] = React.useState(false);
+
+    const [user, setUser] = React.useState({
+        email: "",
+        password: ""
     });
-    const onLogin =()=>{
 
+    React.useEffect(() => {
+        if (user.email.length > 0 && user.password.length > 0) {
+            setButtonDisable(false);
+        } else {
+            setButtonDisable(true);
+        }
+    }, [user]);
+
+    const onLogin = async () => {
+        try {
+            setLoading(true)
+            const res = await axios.post('/api/users/login',user);
+            console.log(res.data);
+            toast.success("Login Successfully");
+            router.push('/profile');
+        } catch (error:any) {
+            toast.error(error.message);
+            console.log(error.message);
+        }finally{
+            setLoading(false);
+        }
     };
-    return(
+
+    return (
         <div className="flex  items-center justify-center py-2 min-h-screen ">
             <div className="flex flex-col max-w-4xl bg-gray-600 px-8 py-6 rounded-md space-y-4">
-                <h1 className="text-2xl text-emerald-400 font-bold  space-y-4 text-center">Login</h1>
-                <hr/>
+                <h1 className="text-2xl text-emerald-400 font-bold  space-y-4 text-center">{loading ? "Loading" : "Login"}</h1>
+                <hr />
                 <div className=" flex flex-col">
                     <label htmlFor="email">Email</label>
-                    <input className="" id="email" value={user.email}
+                    <input className="p-2" id="email" value={user.email}
                         onChange={(e) => setUser({ ...user, email: e.target.value })}
                         placeholder="example12@gmail.com"
                     />
@@ -29,7 +55,7 @@ export default function LoginPage(){
 
                 <div className=" flex flex-col">
                     <label htmlFor="password">Password</label>
-                    <input className="" id="password" value={user.password}
+                    <input className="p-2" id="password" value={user.password}
                         onChange={(e) => setUser({ ...user, password: e.target.value })}
                         placeholder=".........."
                     />
@@ -37,7 +63,7 @@ export default function LoginPage(){
 
                 <button className="p-2 border bg-gray-400 rounded-md focus:border-gray-600
                  focus:outline-none mb-4" onClick={onLogin}>
-                    SignIn
+                    {buttonDisable ? "Can't Login" : "LogIn"}
                 </button>
 
                 <Link href="/signup" >Hit the signIn</Link>
